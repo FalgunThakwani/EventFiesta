2f858aa59cfe9d469b2fd8af0792062309cab3bd
Falgun Jairaj Thakwani <fl700637@dal.ca>
2022-11-16 05:00:27 +0530

    Created Model class for User(Check /model/user.java Added UI screen for user sign up Set up sign up controller

---
+++ src/main/java/group11/EventFiesta/controller/SignUpController.java
@@ -0,0 +1,25 @@
+package group11.EventFiesta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import group11.EventFiesta.model.User;
+
+@Controller
+public class SignUpController {
+    @GetMapping("/signup")
+    public String getHomePage(Model model) {
+        model.addAttribute("user", new User());
+        return "UserSignUp";
+    }
+
+    @PostMapping("/handleUserSignUp")
+    public String handleUserSignUp(@ModelAttribute User user) {
+        // Todo: Store User object in db
+        return "UserSignUp";
+    }
+
+}
---
+++ src/main/java/group11/EventFiesta/model/User.java
@@ -0,0 +1,77 @@
+package group11.EventFiesta.model;
+
+public class User {
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String securityQuestion;
+    private String securityAnswer;
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return this.confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getSecurityQuestion() {
+        return this.securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getSecurityAnswer() {
+        return this.securityAnswer;
+    }
+
+    public void setSecurityAnswer(String securityAnswer) {
+        this.securityAnswer = securityAnswer;
+    }
+
+}
