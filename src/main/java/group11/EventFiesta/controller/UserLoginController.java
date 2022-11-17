84443cee6cebb0e1bbe78f4b824cfa249d705816
Ruchika Ruchika <rc315087@dal.ca>
2022-11-17 15:15:12 -0400

    Added Homepage code. Added User Login code.

---
+++ src/main/java/group11/EventFiesta/controller/UserLoginController.java
@@ -0,0 +1,27 @@
+package group11.EventFiesta.controller;
+
+import group11.EventFiesta.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserLoginController {
+
+        @GetMapping("/userlogin")
+        public String getHomePage(Model model) {
+            model.addAttribute("user", new User());
+            return "UserLogin";
+        }
+
+        @PostMapping("/handleUserLogin")
+        public String handleUserSignUp(@ModelAttribute User user) {
+            // Todo: logic for checking username and password
+
+            return "UserLogin";
+        }
+
+    }
+
