8d77eb60e2a3e7ee157c66681b7afd2f9b404490
dnaik <Dipti@2835>
2022-12-02 12:21:34 -0400

    Organizer Sign Up Module

--- src/main/java/group11/EventFiesta/controller/SignUpController.java
+++ src/main/java/group11/EventFiesta/controller/SignUpController.java
@@ -1,5 +1,6 @@
 package group11.EventFiesta.controller;
 
+import group11.EventFiesta.model.Organizer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,19 @@ public class SignUpController {
         // Todo: Store User object in db
 
         return "UserSignUp";
+    }
+
+    @GetMapping("/org/signup")
+    public String getOrgSignUpPage(Model model){
+        model.addAttribute("organizer", new Organizer());
+        return "OrganizerSignUp";
+    }
+
+    @PostMapping("/handleOrgSignUp")
+    public String handleOrgSignUp(@ModelAttribute Organizer organizer) {
+        //Store Organizer object in db
+
+        return "home";
     }
 
 }
