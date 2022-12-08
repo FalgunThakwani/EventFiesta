1a4f8d210f12bcf9722ea69b66fca5d6148ddfa9
warrier <sr566227@dal.ca>
2022-12-07 21:51:58 -0400

    store password and salt as stringified byte array

--- src/test/java/group11/EventFiesta/EncryptPasswordTest.java
+++ src/test/java/group11/EventFiesta/EncryptPasswordTest.java
@@ -9,8 +9,6 @@ public class EncryptPasswordTest {
 
     @Test
     public void getEncryptedPwd() {
-//        String pwdFromDB = "3DBOFG449Z34j7dTYS+hDA==";
-//        String saltFromDB = "UHbtPc3UnowVcCCd6I+v1g==";
         String saltFromDB = EncryptPassword.getNextSalt();
         String pwd = "Abc@123";
         System.out.println(saltFromDB);
