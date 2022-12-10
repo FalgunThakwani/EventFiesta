e242a462593303d058363ad8b4453b13287a3a8a
warrier <sr566227@dal.ca>
2022-12-10 16:09:38 -0400

    test commented

--- src/test/java/group11/EventFiesta/mail/MailHandlerTest.java
+++ src/test/java/group11/EventFiesta/mail/MailHandlerTest.java
@@ -8,6 +8,6 @@ public class MailHandlerTest {
     public void sendMailtest() {
         MailHandler mailHandler = new SSLMailHandler("smtp.gmail.com", 465);
         Mail mail = new Mail("sreedevi.rw@gmail.com", "Event Fiesta test mail", "This is a test mail!");
-        mail.sendMail(mailHandler);
+//        mail.sendMail(mailHandler);
     }
 }
