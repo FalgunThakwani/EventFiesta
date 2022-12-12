7671562bf480559b77a749a571fb3b446299c2c6
Ruchika Ruchika <rc315087@dal.ca>
2022-12-12 04:31:52 -0400

    Adding UserEventChecklist Test cases to test Todo Checklist

---
+++ src/test/java/group11/EventFiesta/UserEventChecklist/MockUserEventChecklistPersistence.java
@@ -0,0 +1,66 @@
+package group11.EventFiesta.UserEventChecklist;
+
+import group11.EventFiesta.DBConnection.IDBPersistence;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MockUserEventChecklistPersistence implements IDBPersistence
+{
+    ArrayList<HashMap<String, Object>> checklist;
+
+    public MockUserEventChecklistPersistence()
+    {
+        checklist = new ArrayList<>();
+    }
+
+    @Override
+    public ArrayList<HashMap<String, Object>> loadData(String query) throws Exception
+    {
+        HashMap<String, Object> map = new HashMap<>();
+
+        map.put("checklist_item_name", "one");
+        map.put("status", 1);
+        map.put("checklist_item_id", 1);
+
+        checklist.add(map);
+
+        return checklist;
+    }
+
+    @Override
+    public ArrayList<HashMap<String, Object>> loadData(String storedProcedure, Object... params) throws Exception
+    {
+        HashMap<String, Object> map = new HashMap<>();
+
+        map.put("checklist_item_name", "one");
+        map.put("status", 1);
+        map.put("checklist_item_id", 1);
+
+        checklist.add(map);
+
+        return checklist;
+    }
+
+    @Override
+    public Integer saveData(String query, Object... params) throws Exception
+    {
+        HashMap<String, Object> map = new HashMap<>();
+
+        map.put("checklist_item_name", "two");
+        map.put("status", 1);
+        map.put("checklist_item_id", 2);
+
+        checklist.add(map);
+
+        return 1;
+    }
+
+    @Override
+    public Integer updateData(String storedProcedure, Object... params) throws Exception
+    {
+        checklist.get(0).put("status", 0);
+        return 1;
+    }
+}
